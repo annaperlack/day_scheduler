b30998ca1e3cd7ee84dc1fb525c62ed33e2f078a
Anna Perlack <annaperlack@gmail.com>
2022-11-22 12:55:53 -0500

    added work hours

--- Develop/script.js
+++ Develop/script.js
@@ -38,5 +38,21 @@ WHEN I refresh the page
 THEN the saved events persist
 */
 
+// 8AM-5PM work hours
+var workHours = [
+  '8:00 AM',
+  '9:00 AM',
+  '10:00 AM',
+  '11:00 AM',
+  '12:00 PM',
+  '1:00 PM',
+  '2:00 PM',
+  '3:00 PM',
+  '4:00 PM',
+  '5:00 PM',
+];
+
+
+
 var today = dayjs();
 $('#currentDay').text(today.format('MMMM D, YYYY'));
